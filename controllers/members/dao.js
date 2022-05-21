6915af7627801899c677befc780860e274f65e91
Alan Galvan <alexis_120@live.com.ar>
2022-05-21 17:02:10 -0300

    Endpoint Remove Members

--- controllers/members/dao.js
+++ controllers/members/dao.js
@@ -3,5 +3,17 @@ const db = require("../../models");
 module.exports = {
     async createMemberDAO(member) {
         await db.Members.create(member);
+    },
+    async deleteMemberDAO(memberID){
+        console.log("eee")
+        return await db.Members.destroy({
+            where:{id:memberID}
+        }
+/*         await db.Members.destroy({where:{id:memberID}}) */
+    )},
+    async getMemberDAO(id){
+        return await db.Members.findOne({
+            where:{id:id}
+        })
     }
 }
--- routes/members.js
+++ routes/members.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createMember } = require('../controllers/members/members.js');
+const { createMember, getMember, deleteMember } = require('../controllers/members/members.js');
 const {validateToken,generateToken}= require('../middlewares/auth');
 
 router.post("/",validateToken,createMember);
+router.get("/:id",getMember);
+router.delete("/:id",deleteMember);
+
 router.get("/",generateToken,(req,res)=>{
     res.json({ok:"ok"})
 });
