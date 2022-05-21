204f1c8cd4256ec90566e175a96c065d7458f60f
Alan Galvan <alexis_120@live.com.ar>
2022-05-21 17:02:10 -0300

    Endpoint Remove Members

--- controllers/members/members.js
+++ controllers/members/members.js
@@ -1,22 +1,47 @@
-const { createMemberDAO } = require("./dao");
+const { getMemberDAO, deleteMemberDAO, createMemberDAO } = require("./dao");
 
 module.exports = {
-    async createMember(req,res){
+    async createMember(req, res) {
         const name = req.body.name;
-        if(!name && typeof name !== 'string' ) 
-        {
+        if (!name && typeof name !== 'string') {
             return res.status(400).json({
-                ok:false,
-                menssage:"Name is required or type invalid!"
+                ok: false,
+                menssage: "Name is required or type invalid!"
             })
         }
         const receivedMember = {
-            name:req.body.name,
-            image:req.body.image
+            name: req.body.name,
+            image: req.body.image
         }
         const memberCreated = await createMemberDAO(receivedMember);
         return res.status(201).json({
-            user:memberCreated
+            user: memberCreated
         })
+    },
+    async deleteMember(req, res) {
+        const memberID = await getMemberDAO(req.params.id);
+        console.log("memberID ",memberID)
+        if (!memberID) {
+            return res.status(404).json({
+                message: "Member not exist"
+            });
+        }
+        try {
+            await deleteMemberDAO(memberID);
+            return res.status(200).json({
+                menssage: "Member Deleted"
+            })
+        } catch (error) {
+            return res.status(500).json({
+                menssage:"Internal server error"
+            })
+        }
+    },
+    async getMember(req, res) {
+        const memberID = await getMemberDAO(req.params.id);
+        if (!memberID) return res.status(404).json({
+            message: "Member not found"
+        });
+
     }
 }
