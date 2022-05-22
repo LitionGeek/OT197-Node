48542db90ec001c26c77050573b7a288b44862d8
guillote87 <guillermo27@gmail.com>
2022-05-21 22:11:07 -0300

    OT197-24 Y OT197-33 Creadas el endpoint de registro y eliminacion de usuarios

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const regValidations = require('../middlewares/regValidations')
+const usersController = require  ('../controllers/users/usersController')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* POST user create. */
+router.post('/auth/register',regValidations ,usersController.create)
+
+
+/* DELETE user delete. */
+router.delete('/:id', usersController.delete);
 module.exports = router;
