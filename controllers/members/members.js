0aed772c6cdaec19c109804dc273011cb466a095
Alan Galvan <alexis_120@live.com.ar>
2022-05-23 18:13:09 -0300

    Added trycatch in createMember

--- controllers/members/members.js
+++ controllers/members/members.js
@@ -13,27 +13,38 @@ module.exports = {
             name: req.body.name,
             image: req.body.image
         }
-        const memberCreated = await createMemberDAO(receivedMember);
-        return res.status(201).json({
-            user: memberCreated
-        })
+        try {
+            const memberCreated = await createMemberDAO(receivedMember);
+            return res.status(201).json({
+                user: memberCreated
+            })
+        } catch (error) {
+            return res.status(500).json({
+                menssage: error
+            })
+        }
     },
     async deleteMember(req, res) {
         const memberID = await getMemberDAO(req.params.id);
-        console.log("memberID ",memberID)
         if (!memberID) {
             return res.status(404).json({
                 message: "Member not exist"
             });
         }
         try {
+            const memberID = await getMemberDAO(req.params.id);
+            if (!memberID) {
+                return res.status(404).json({
+                    message: "Member not exist"
+                });
+            }
             await deleteMemberDAO(memberID);
             return res.status(200).json({
                 menssage: "Member Deleted"
             })
         } catch (error) {
             return res.status(500).json({
-                menssage:"Internal server error"
+                menssage: error
             })
         }
     },
