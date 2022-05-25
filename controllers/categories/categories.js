f0a3c65a300e663ff3ae76cb174b18ae96001831
SFRisso <santiago.risso2@gmail.com>
2022-05-25 17:52:52 -0300

    fixed frasing on error messages and variables names

--- controllers/categories/categories.js
+++ controllers/categories/categories.js
@@ -7,7 +7,7 @@ const create = async(req, res) => {
         //check if unique
         const error = await uniqueName(category.name)
         if (error) {
-            res.json({ error: 'Category name is not unique.' });
+            res.json({ error: 'Category must be unique.' });
             throw error
         }
         res.status(201).json(await Categories.create(req.body));
@@ -26,7 +26,7 @@ const remove = async(req, res) => {
     let category = await Categories.destroy({
         where: { id: req.params.idCat }
     });
-    if (pers == 0) {
+    if (category == 0) {
         res.status(400).json({ error: 'Category not found.' });
     } else {
         res.status(200).json({ success: `Category ${req.params.id} removed succesfully.` })
@@ -39,11 +39,11 @@ const update = async(req, res) => {
     //check if unique
     const error = await uniqueName(category.name)
     if (error) {
-        res.json({ error: 'Category name is not unique.' });
+        res.json({ error: 'Category must be unique.' });
         throw error
     }
     let category = await Categories.update(req.body, { where: { id: req.params.idCat } });
-    if (pers == 0) {
+    if (category == 0) {
         res.status(400).json({ error: 'Category not found or update information empty.' });
     } else {
         res.status(200).json({ success: `The member ${req.params.id} was updated succesfully.`, data: category })
@@ -54,7 +54,7 @@ const update = async(req, res) => {
 //Get All
 const getAll = async(req, res) => {
     let category = await Categories.findAll();
-    if (Object.keys(pers).length === 0) {
+    if (Object.keys(category).length === 0) {
         res.status(400).json({ error: 'Category database is empty.' });
     } else {
         res.status(200).json(category);
@@ -65,7 +65,7 @@ const getAll = async(req, res) => {
 const getById = async(req, res) => {
     try {
         let category = Categories.findByPk(req.params.idCat);
-        if (Object.keys(pers).length === 0) {
+        if (Object.keys(category).length === 0) {
             res.status(400).json({ error: 'Category not found.' });
         } else {
             res.status(200).json(category);
