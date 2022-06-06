3c56463f5b8e15fc1d8b4e03fb9d3fd9bca029cb
JonaPortillo <portillo.jonathan.leonel@gmail.com>
2022-06-06 19:39:37 -0300

    controller, model and migrations, from Testimonials created

---
+++ controllers/testimonials/testimonials.js
@@ -0,0 +1,6 @@
+const db = require("../../models");
+const { validationResult } = require('express-validator');
+
+module.exports = {
+    /* Insert function of controller (create, delete, edit, etc.) */
+}
