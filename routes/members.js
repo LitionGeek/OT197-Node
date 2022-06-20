3b5fcb5d23155990e39106993efbb2cfc7b113bf
Alan Galvan <alexis_120@live.com.ar>
2022-06-20 18:42:39 -0300

    Se agregan las vacidaciones el los endpoints de members

--- routes/members.js
+++ routes/members.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { createMember, deleteMember, getAllMembers, editMember } = require('../controllers/members/members.js');
 const {validateToken,generateToken}= require('../middlewares/auth');
-const { validateNameMember } = require('../middlewares/validationsMembers.js');
+const { validateMember } = require('../middlewares/validationsMembers.js');
 
 
 router.get("/:page",getAllMembers);
-router.post("/",[validateNameMember/*,validateToken*/],createMember);
-router.delete("/:id",/*validateToken,*/deleteMember);
-router.put("/:id",validateNameMember,editMember);
+router.post("/",validateMember,validateToken,createMember);
+router.delete("/:id",validateToken,deleteMember);
+router.put("/:id",validateMember,editMember);
 
 module.exports = router;
