15209eaf1ab8ae562915510a1f00002b4f9daf9c
Alan Galvan <alexis_120@live.com.ar>
2022-06-20 18:44:20 -0300

    Se agregan los test cases de members

--- tests/members.test.js
+++ tests/members.test.js
@@ -25,7 +25,7 @@ const members = [
     }, {
         name: 213
     }, {
-        name: ''
+        name: 'alan'
     }
 ];
 
@@ -33,12 +33,15 @@ let token;
 
 describe('Members', () => {
     /**
-    * Delete organizations in the table
+    * Delete members in the table
     */
-/*     before(async () => { //Before each test we empty the database
-        await db.Members.destroy({ truncate: true });
-    });
- */
+        before(async () => { //Before each test we empty the database
+            await db.Members.destroy({
+                where: {},
+                truncate: true
+              });         
+        }); 
+
     /*
     * Test the /POST auth user
     */
@@ -51,7 +54,6 @@ describe('Members', () => {
                     token = res.body.token;
                     res.should.have.status(200);
                     res.body.should.have.property("token");
-                    /*       res.body.should.have.property("user").eql("roleId"); */
                 });
         });
 
@@ -59,7 +61,7 @@ describe('Members', () => {
 
     });
 
-    /* GET ALL ORGANIZATION */
+    /* GET ALL MEMBER */
     describe('/GET MEMBERS', () => {
         it('Test 2- it should GET all the members', async () => {
             await chai.request(server)
@@ -75,8 +77,8 @@ describe('Members', () => {
         });
     });
 
-    /* POST CREATE ORGANIZATION */
-    describe('/POST organization', () => {
+    /* POST CREATE MEMBER */
+    describe('/POST MEMBER', () => {
         it('Test 3- it should Create member', async () => {
             await chai.request(server)
                 .post('/members')
@@ -91,21 +93,121 @@ describe('Members', () => {
     });
 
 
-    /* PUT ORGANIZATION with TOKEN*/
+    /* PUT MEMBER with TOKEN*/
     describe('/PUT/:id member', () => {
         it('Test 4- it should PUT ID member', async () => {
             const count = await db.Members.max('id', { attributes: ['id'] })
             await chai.request(server)
-                .put(`/member/${count}`)
+                .put(`/members/${count}`)
                 .set("Authorization", token)
                 .send(members[1])
                 .then((res) => {
-                    console.log('Count ',count)
                     res.should.have.status(200);
-                    res.body.should.have.property("message").eql('Member Updated');
+                    res.body.should.have.property("message").eql('Member update');
                 })
         });
     });
 
+    /* DELETE MEMBER with TOKEN*/
+    describe('/DELETE/:id member', () => {
+        it('Test 5- it should DELETE ID member', async () => {
+            const count = await db.Members.max('id', { attributes: ['id'] })
+            await chai.request(server)
+                .delete(`/members/${count}`)
+                .set("Authorization", token)
+                .then((res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property("message").eql('Member Deleted');
+                })
+        });
+    });
+
+    /* POST CREATE MEMBER SIN VALORES REQUERIDOS */
+    describe('/POST member', () => {
+        it('Test 6- it should Create member with name in number', async () => {
+            await chai.request(server)
+                .post('/members')
+                .set("Authorization", token)
+                .send(members[2])
+                .then((res) => {
+                    res.should.have.status(400);
+                    res.body.should.have.property("message").eql("Name is required and type string");
+                });
+        });
+    });
+
+    /* POST CREATE MEMBER SIN VALORES REQUERIDOS */
+    describe('/POST member', () => {
+        it('Test 7- it should Create member with name valid and image null', async () => {
+            await chai.request(server)
+                .post('/members')
+                .set("Authorization", token)
+                .send(members[3])
+                .then((res) => {
+                    res.should.have.status(400);
+                    res.body.should.have.property("message").eql("Image is required");
+                });
+        });
+    });
+
+    
+
+    /* POST CREATE MEMBER SIN VALORES REQUERIDOS */
+    describe('/PUT member', () => {
+        it('Test 8- it should EDIT member with name in number', async () => {
+            const count = await db.Members.max('id', { attributes: ['id'] })
+            await chai.request(server)
+                .put(`/members/${count}`)
+                .set("Authorization", token)
+                .send(members[2])
+                .then((res) => {
+                    res.should.have.status(400);
+                    res.body.should.have.property("message").eql("Name is required and type string");
+                });
+        });
+    });
+
+    /* POST CREATE MEMBER SIN VALORES REQUERIDOS */
+    describe('/PUT member', () => {
+        it('Test 9- it should PUT member with name valid and image null', async () => {
+            const count = await db.Members.max('id', { attributes: ['id'] })
+            await chai.request(server)
+                .put(`/members/${count}`)
+                .set("Authorization", token)
+                .send(members[3])
+                .then((res) => {
+                    res.should.have.status(400);
+                    res.body.should.have.property("message").eql("Image is required");
+                });
+        });
+    });
+
+    describe('/DELETE/:id member', () => {
+        it('Test 10- it should DELETE ID member not exist', async () => {
+            const count = await db.Members.max('id', { attributes: ['id'] })
+            await chai.request(server)
+                .delete(`/members/${count+1}`)
+                .set("Authorization", token)
+                .then((res) => {
+                    res.should.have.status(404);
+                    res.body.should.have.property("message").eql('Member not exist');
+                })
+        });
+    });
+
+       /* POST CREATE MEMBER SIN VALORES REQUERIDOS */
+       describe('/PUT member', () => {
+        it('Test 11- it should PUT member with name valid and image null', async () => {
+            const count = await db.Members.max('id', { attributes: ['id'] })
+            await chai.request(server)
+                .put(`/members/${count+1}`)
+                .set("Authorization", token)
+                .send(members[0])
+                .then((res) => {
+                    res.should.have.status(404);
+                    res.body.should.have.property("message").eql("Member not found");
+                });
+        });
+    });
 
 });
